0fca8b426a6db2ee3048bddf8d691cfe48d00579
root <kehinde.o.obanla@gmail.com>
2021-10-25 19:41:52 +0000

    Ass7 get

--- Assignment7/main.py
+++ Assignment7/main.py
@@ -21,6 +21,21 @@ def Timetwenty(converted):
     out_time = datetime.strftime(in_time, "%H:%M")
     return out_time
 
+def formatResult(res):
+    result = res['data']
+    if(len(result) ==0):
+        return 'invalid subject'
+    else:
+
+        response =[]
+        for courses in result:
+            if('Begin' in courses):
+                courses['Begin'] = Timetwelve(str(courses['Begin']))
+            if('End' in courses):
+                courses['End'] = Timetwelve(str(courses['End']))
+            response.append(courses)
+        return response
+
 class Time(BaseModel):
     Begin:str
     End: Optional[str] = None
@@ -42,6 +57,12 @@ class filter(BaseModel):
         End: Optional[str] = None
         Bldg:  Optional[str] = None
         Room: Optional[str] = None
+
+class AdvisingForms(BaseModel):
+    StudentID:Optional[str] = None
+    Semester:Optional[str] = None
+    Year:Optional[str] = None
+
 
 
 """ Api """
@@ -70,15 +91,7 @@ async def Subj(Subj :str):
     if(len(result) ==0):
         return 'invalid subject'
     else:
-
-        response =[]
-        for courses in result:
-            if('Begin' in courses):
-                courses['Begin'] = Timetwelve(str(courses['Begin']))
-            if('End' in courses):
-                courses['End'] = Timetwelve(str(courses['End']))
-            response.append(courses)
-        return response
+        return formatResult(res)
 @app.get("/courses/Crse/{Crse}")
 async def Crse(Crse  :str):
     sql = f'SELECT * FROM `CourseInfo` WHERE `Crse` ="{Crse}" ORDER BY `CourseInfo`.`year` DESC'
@@ -87,14 +100,7 @@ async def Crse(Crse  :str):
     if(len(result) ==0):
         return 'invalid course number'
     else:
-        response =[]
-        for courses in result:
-            if('Begin' in courses):
-                courses['Begin'] = Timetwelve(str(courses['Begin']))
-            if('End' in courses):
-                courses['End'] = Timetwelve(str(courses['End']))
-            response.append(courses)
-        return response
+        return formatResult(res)
 @app.get("/courses/Instructor/{name}")
 async def Instructor(name :str):
     newname = name.lower() +'%'
@@ -104,14 +110,7 @@ async def Instructor(name :str):
     if(len(result) ==0):
         return 'invalid name'
     else:
-        response =[]
-        for courses in result:
-            if('Begin' in courses):
-                courses['Begin'] = Timetwelve(str(courses['Begin']))
-            if('End' in courses):
-                courses['End'] = Timetwelve(str(courses['End']))
-            response.append(courses)
-        return response
+        return formatResult(res)
 @app.get("/courses/building/{Bldg}")
 async def building(Bldg :str):
     sql = f'SELECT * FROM `CourseInfo` WHERE `Bldg`= "{Bldg}" ORDER BY `CourseInfo`.`year` DESC'
@@ -120,14 +119,7 @@ async def building(Bldg :str):
     if(len(result) ==0):
         return 'invalid building'
     else:
-        response =[]
-        for courses in result:
-            if('Begin' in courses):
-                courses['Begin'] = Timetwelve(str(courses['Begin']))
-            if('End' in courses):
-                courses['End'] = Timetwelve(str(courses['End']))
-            response.append(courses)
-        return response
+        return formatResult(res)
 
 
 @app.get("/courses/Time/{Tyme}")
@@ -147,14 +139,7 @@ async def ClassTyme(Tyme:str):
         if(len(result) ==0):
             return 'invalid Time'
         else:
-            response =[]
-            for courses in result:
-                if('Begin' in courses):
-                    courses['Begin'] = Timetwelve(str(courses['Begin']))
-                if('End' in courses):
-                    courses['End'] = Timetwelve(str(courses['End']))
-                response.append(courses)
-            return response
+            return formatResult(res)
 @app.get("/courses/closed/")
 async def Classclosed(): 
     sql =f'SELECT * FROM `CourseInfo` WHERE `Aval` = 0 ORDER BY `CourseInfo`.`year` DESC' 
@@ -163,37 +148,41 @@ async def Classclosed():
     if(len(result) ==0):
         return 'no available class'
     else:
-        response =[]
-        for courses in result:
-            if('Begin' in courses):
-                courses['Begin'] = Timetwelve(str(courses['Begin']))
-            if('End' in courses):
-                courses['End'] = Timetwelve(str(courses['End']))
-            response.append(courses)
-        return response  
+        return formatResult(res)  
 
 @app.get("/courses/Title/{title}")
 async def Instructor(title :str):
     newtitle = '%' + title.lower() +'%'
-    sql = f'SELECT * FROM `CourseInfo` WHERE `Title` LIKE "{newtitle }" ORDER BY `CourseInfo`.`year` DESC'
+    sql = f'SELECT * FROM `CourseInfo` WHERE `Title` LIKE "{newtitle}" ORDER BY `CourseInfo`.`year` DESC'
     res = cnx.query(sql)
     result = res['data']
     if(len(result) ==0):
         return 'invalid Title'
     else:
-        response =[]
-        for courses in result:
-            if('Begin' in courses):
-                courses['Begin'] = Timetwelve(str(courses['Begin']))
-            if('End' in courses):
-                courses['End'] = Timetwelve(str(courses['End']))
-            response.append(courses)
-        return response
+        return formatResult(res)
+
+@app.get("/courses/buildingndroom/{buldroom}")
+async def ClassTyme(buldroom:str):
+    SplytTyme = buldroom.split(',')
+    Bldg = SplytTyme[0]
+    if(len(SplytTyme) >1):
+        Room = SplytTyme[1]
+        newRoom = '%' + Room +'%'
+    else:
+        Room =" "
+    if(Room != " "):
+        sql =f'SELECT * FROM `CourseInfo` WHERE `Bldg` = "{Bldg}" And `Room` LIKE "{newRoom}" ORDER BY `CourseInfo`.`year` DESC'
+        res = cnx.query(sql)
+        result = res['data']
+        if(len(result) ==0):
+            return 'invalid room or building'
+        else:
+            return formatResult(res)
 
 @app.post("/Annony")
 async def filterall(sqlList:filter):
-    print(sqlList.Col)
     sql ="SELECT * FROM `CourseInfo` WHERE year = 2022"
+    
     response ={}
     if(sqlList.Col!=None):
         response['Col'] = sqlList.Col
@@ -235,11 +224,119 @@ async def filterall(sqlList:filter):
     if(len(result) ==0):
         return 'no available class'
     else:
-        response =[]
-        for courses in result:
-            if('Begin' in courses):
-                courses['Begin'] = Timetwelve(str(courses['Begin']))
-            if('End' in courses):
-                courses['End'] = Timetwelve(str(courses['End']))
-            response.append(courses)
-        return response
+        return formatResult(res)
+
+@app.get("/student")
+async def Allstudent():
+    sql ='SELECT * FROM `StudentIInfo`'
+    res = cnx.query(sql)
+    
+    return formatResult(res)
+
+@app.get("/student/Mnumber/{Mnumber}")
+async def StudentMnumber (Mnumber:str):
+    sql =f'SELECT * FROM `StudentIInfo` WHERE `Mnumber` ="{Mnumber}"'
+    res = cnx.query(sql)
+    result = res['data']
+    if(len(result) ==0):
+        return 'Invalid Mnumber'
+    else:
+        return formatResult(res)
+@app.get("/student/Name/{Name}")
+async def studentName (Name:str):
+    splitname =Name.split(',')
+    Fname = splitname[0]
+    Lname = splitname[1]
+    sql =f'SELECT * FROM `StudentIInfo` WHERE `FirstName`="{Fname}"and `LastName`="{Lname}"'
+    res = cnx.query(sql)
+    result = res['data']
+    if(len(result) ==0):
+        return 'Invalid name'
+    else:
+        return formatResult(res)
+
+@app.get("/student/GPAG/{GPA}")
+async def GPAG (GPA:int):
+    sql =f'SELECT * FROM `StudentIInfo` WHERE `GPA` >= "{GPA}"'
+    res = cnx.query(sql)
+    result = res['data']
+    if(len(result) ==0):
+        return 'Invalid GPA'
+    else:
+        return formatResult(res)
+
+@app.get("/student/GPAL/{GPA}")
+async def GPAL(GPA:int):
+    sql =f'SELECT * FROM `StudentIInfo` WHERE `GPA` <= "{GPA}"'
+    res = cnx.query(sql)
+    result = res['data']
+    if(len(result) ==0):
+        return 'Invalid GPA'
+    else:
+        return formatResult(res)
+
+@app.get("/Advising")
+async def Advising ():
+    sql =f'SELECT * FROM `Advisingform`'
+    res = cnx.query(sql)
+    
+    return formatResult(res)
+
+@app.get("/Advising/student/{Mnumber}")
+async def AdvisingStudentnum(Mnumber:str):
+    sql =f'SELECT * FROM `Advisingform` WHERE `StudentID` ="{Mnumber}"'
+    res = cnx.query(sql)
+    result = res['data']
+    if(len(result) ==0):
+        return 'Invalid number'
+    else:
+        return formatResult(res)
+
+@app.get("/Advising/Semester/{Semester}")
+async def AdvisingStudentSemester (Semester:str):
+    sql =f'SELECT * FROM `Advisingform` WHERE `Semester`="{Semester}"'
+    res = cnx.query(sql)
+    result = res['data']
+    if(len(result) ==0):
+        return 'Invalid Semester'
+    else:
+        return formatResult(res)
+@app.get("/Advising/year/{year}")
+async def AdvisingStudentyear (Semester:str):
+    sql =f'SELECT * FROM `Advising form` WHERE `Year`="{year}"'
+    res = cnx.query(sql)
+    result = res['data']
+    if(len(result) ==0):
+        return 'Invalid Year'
+    else:
+        return formatResult(res)
+
+@app.post("/Advising/All")
+async def filterAdvisform(Advisform:AdvisingForms):
+    count =0
+    response ={}
+    sql ="SELECT * FROM `Advisingform` WHERE"
+    if(Advisform.StudentID!=None):
+        response['StudentID'] = Advisform.StudentID
+    if(Advisform.Semester!=None):
+        response['Semester'] = Advisform.Semester
+    if(Advisform.Year!=None):
+        response['Year'] = Advisform.Year
+    if(len(response)>1):
+        for x,y in response.items():
+            sql = sql + " " + '`' + x +'`'  + " " + "="+ " "+ "'" +str(y) +"'"  
+            del response[x]
+            break
+        for x,y in response.items():
+            addand = 'AND'
+            sql = sql + addand + " " + '`' + x +'`'  + " " + "="+ " "+ "'" +str(y) +"'"      
+    else:
+        for x,y in response.items():
+            sql = sql + " " + '`' + x +'`'  + " " + "="+ " "+ "'" +str(y) +"'"  
+    sql = sql + ";"
+    res = cnx.query(sql)
+    result = res['data']
+    if(len(result) ==0):
+        return 'Invalid Advising filters'
+    else:
+        return formatResult(res)
