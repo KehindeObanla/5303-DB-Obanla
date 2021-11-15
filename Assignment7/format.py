f49398a45da1cad70e00e168be8ce95ba02fdefe
KehindeObanla <kehinde.o.obanla@gmail.com>
2021-11-15 21:49:42 +0000

    db edit

--- Assignment7/format.py
+++ Assignment7/format.py
@@ -41,10 +41,9 @@ with open('/var/www/html/Database/.config.json') as f:
     config = json.loads(f.read())
 
 cnx = MysqlCnx(**config)
-col = set()
-sql = "SELECT `Col` FROM `CourseInfo`"
-res = cnx.query(sql)
-
+student ={
+    "FirstName" =
+}
 """ coursedic = {
         "Col": "",
         "Crn": "",
@@ -63,7 +62,7 @@ res = cnx.query(sql)
         "Room": " "
 
 }
-f = open('/var/www/html/Database/Assignment7/summer2_2021.json')
+f = open('/var/www/html/Database/Assignment7/2021_summer_1_schedule.json')
 
 data = json.load(f)
 count =0
@@ -71,8 +70,12 @@ count2 =0
 test =[]
 for i in data:
     dic = i
+    
     if("Crn" not in dic):
         test.append(i)
+        count2+=1
+        continue
+    if(type(dic['Crn'])!= str):
         count2+=1
         continue
     if('-' in dic['Crn'] or dic['Crn'] == '' ):
@@ -107,9 +110,15 @@ for i in data:
         if("Days" in dic):
             coursedic['Days'] = dic['Days']
         if("Begin" in dic):
-            coursedic['Begin'] =  Timetwenty(convertTime(dic['Begin']))
+            if('am' in dic['Begin'] or 'pm' in dic['Begin'] ):
+                coursedic['Begin'] =  Timetwenty(convertTime(dic['Begin']))
+            else:
+                coursedic['Begin']=" "
         if("End" in dic):
-            coursedic['End'] =  Timetwenty(convertTime(dic['End']))
+            if('am' in dic['End'] or 'pm' in dic['End'] ):
+                coursedic['End'] =  Timetwenty(convertTime(dic['End']))
+            else:
+                coursedic['End'] =" "
         if("Bldg" in dic):
             coursedic['Bldg'] =  dic['Bldg']
         if("Room" in dic):
@@ -118,13 +127,14 @@ for i in data:
         count+=1
         
         
-        sql ="INSERT INTO `CourseInfo` (`Col`, `Crn`, `Subj`, `Crse`, `Sect`, `Title`, `PrimaryInstructor`, `Max`, `Curr`, `Aval`, `Days`, `Begin`, `End`, `Bldg`, `Room`, `year`, `Season`) VALUES ('{Col}', '{Crn}','{Subj}', '{Crse}','{Sect}','{Title}','{PrimaryInstructor}', '{Max}', '{Curr}', '{Aval}', '{Days}', '{Begin}', '{End}', '{Bldg}', '{Room}', '2021', 'Summer2');".format(**coursedic)
+        sql ="INSERT INTO `CourseInfo` (`Col`, `Crn`, `Subj`, `Crse`, `Sect`, `Title`, `PrimaryInstructor`, `Max`, `Curr`, `Aval`, `Days`, `Begin`, `End`, `Bldg`, `Room`, `year`, `Season`) VALUES ('{Col}', '{Crn}','{Subj}', '{Crse}','{Sect}','{Title}','{PrimaryInstructor}', '{Max}', '{Curr}', '{Aval}', '{Days}', '{Begin}', '{End}', '{Bldg}', '{Room}', '2021', 'Spring');".format(**coursedic)
         cnx.query(sql)
+        
 print(len(data))
 print(len(test))
 print(count)
-print(count)    
- """
+print(count2)"""
+
    
 
 
